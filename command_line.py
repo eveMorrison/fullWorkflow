1e090f71fa87036f8e4b7f96b844066aba2774a0
Eva Morrison <eva.morrison33@ku.edu>
2022-01-20 12:41:51 -0600

    update for the full nanopore library

--- command_line.py
+++ command_line.py
@@ -6,7 +6,7 @@ os.chdir(input_dir)
 
 # filter repeatmasker output
 
-awk_filter_nano = "awk '{if (($7-$6) >= 250) print $0;}' nanoporeParis.fa.out | awk '{if($2<=20.0) print $0;}' > nanoporeParis_filter.fa.out"
+awk_filter_nano = "awk '{if (($7-$6) >= 250) print $0;}' dys_60000.fa.out | awk '{if($2<=20.0) print $0;}' > dys_60000_filter.fa.out"
 awk_filter_160 = "awk '{if (($7-$6) >= 250) print $0;}' strain160all.fasta.out | awk '{if($2<=20.0) print $0;}' > strain160all_filter.fa.out"
 awk_filter_9 = "awk '{if (($7-$6) >= 250) print $0;}' strain9all.fasta.out | awk '{if($2<=20.0) print $0;}' > strain9all_filter.fa.out"
 
@@ -16,15 +16,19 @@ os.system(awk_filter_9)
 
 # Find the reads containing the TE
 
-awk_find_nano = '''awk '{if (($10) == "Paris") print $0;}' nanoporeParis_filter.fa.out | awk '{Nanopore[$5];} END{for (var in Nanopore) print var;} ' > nanopore_reads_Paris.txt'''
+awk_find_nano = '''awk '{if (($10) == "Paris") print $0;}' dys_60000_filter.fa.out | awk '{Nanopore[$5];} END{for (var in Nanopore) print var;} ' > nanopore_reads_Paris.txt'''
+remove = "rm dys_60000_filter.fa.out"
 
 os.system(awk_find_nano)
+os.system(remove)
 
 # isolate the reads containing the TE
 
-awk_extract_reads = "awk -F'>' 'NR==FNR{ids[$0]; next} NF>1{f=(substr($2,1,36) in ids)} f' nanopore_reads_Paris.txt nanoporeParis.fa.masked > nanoporeParisOnly.fa.masked"
+awk_extract_reads = "awk -F'>' 'NR==FNR{ids[$0]; next} NF>1{f=(substr($2,1,36) in ids)} f' nanopore_reads_Paris.txt dys_60000.fa.masked > dys_60000_paris.fa.masked"
+remove = "rm nanopore_reads_Paris.txt"
 
 os.system(awk_extract_reads)
+os.system(remove)
 
 # isolate the TE locations in the assemblies
 
@@ -41,23 +45,23 @@ bwa_index_9 = "bwa index strain9all.fasta.masked"
 
 # Normalize Nanaopore Reads to Assemblies
 
-bwa_mem_160 = "bwa mem strain160all.fasta.masked nanoporeParisOnly.fa.masked > nanoporeParisNormalized160.sam"
-bwa_mem_9 = "bwa mem strain9all.fasta.masked nanoporeParisOnly.fa.masked > nanoporeParisNormalized9.sam"
+bwa_mem_160 = "bwa mem strain160all.fasta.masked dys_60000_paris.fa.masked > dys_60000Normalized160.sam"
+bwa_mem_9 = "bwa mem strain9all.fasta.masked dys_60000_paris.fa.masked > dys_60000Normalized9.sam"
 
 # filter sam files
 
-samtools_view_160 = "samtools view -q 50 -F 0x800 -b nanoporeParisNormalized160.sam > nanoporeParisFilter160.bam"
-samtools_view_9 = "samtools view -q 50 -F 0x800 -b nanoporeParisNormalized9.sam > nanoporeParisFilter9.bam"
+samtools_view_160 = "samtools view -q 50 -F 0x800 -b dys_60000_Normalized160.sam > dys_60000_Filter160.bam"
+samtools_view_9 = "samtools view -q 50 -F 0x800 -b dys_60000_Normalized9.sam > dys_60000_Filter9.bam"
 
 # sort sam files for bedfile comparison
 
-samtools_sort_160 = "samtools sort nanoporeParisFilter160.bam -o nanoporeParisSorted160.bam"
-samtools_sort_9 = "samtools sort nanoporeParisFilter9.bam -o nanoporeParisSorted9.bam"
+samtools_sort_160 = "samtools sort dys_60000_Filter160.bam -o dys_60000_Sorted160.bam"
+samtools_sort_9 = "samtools sort dys_60000_Filter9.bam -o dys_60000_Sorted9.bam"
 
 # convert bam file to a bed file
 
-bedtools_bamtobed_160 = "bedtools bamtobed -i nanoporeParisSorted160.bam > nanoporeParis160.bed"
-bedtools_bamtobed_9 = "bedtools bamtobed -i nanoporeParisSorted9.bam > nanoporeParis9.bed"
+bedtools_bamtobed_160 = "bedtools bamtobed -i dys_60000_Sorted160.bam > dys_60000_160.bed"
+bedtools_bamtobed_9 = "bedtools bamtobed -i dys_60000_Sorted9.bam > dys_60000_9.bed"
 
 # convert assmebly files to bed files
 
@@ -66,5 +70,5 @@ awk_tobed_9 = '''awk '{OFS="\t"; if($6>1000) print $5,$6-1000, $7+1000,$10; else
 
 # find inverse intersections between assembly and nanopore
 
-bedtools_intersect_160 = "bedtools intersect -a nanoporeParis160.bed -b strain160all.paris.extended.bed -v > insertParis160.bed"
-bedtools_intersect_9 = "bedtools intersect -a nanoporeParis9.bed -b strain9all.paris.extended.bed -v > insertParis9.bed"
+bedtools_intersect_160 = "bedtools intersect -a dys_60000_160.bed -b strain160all.paris.extended.bed -v > insertParis160.bed"
+bedtools_intersect_9 = "bedtools intersect -a dys_60000_9.bed -b strain9all.paris.extended.bed -v > insertParis9.bed"
